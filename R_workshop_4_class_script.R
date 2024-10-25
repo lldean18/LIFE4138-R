795768a250a73ff131892b21421ce7d82607c529
Laura Dean <43610011+lldean18@users.noreply.github.com>
2024-10-25 11:20:34 +0100

    Update R_workshop_4_class_script.R

--- R_workshop_4_class_script.R
+++ R_workshop_4_class_script.R
@@ -94,4 +94,63 @@ write.vcf(chrom, mask = T, "filtered_stickleback_snps_2.vcf.gz")
 
 
 
+# section 2 PCA 
+
+# read in the hoverfly data
+vcf2 <- read.vcfR("Volucella_bombylans.vcf.gz")
+
+# extract the genotype matrix
+mat <- extract.gt(vcf2)
+
+mat
+
+# we need to transpose our matrix
+mat <- t(mat)
+mat
+
+gen <- df2genind(mat, sep = "/", ind.names = row.names(mat))
+gen
+
+# calculate allele frequencies
+allele_freqs <- tab(gen, freq = T, NA.method = "mean")
+
+# perform PCA
+pca <- dudi.pca(allele_freqs, center = T, scale = F)
+
+pca <- dudi.pca(df = allele_freqs, center = T, scale = F, scannf = FALSE, nf = 3)
+
+
+summary(pca)
+
+# access the PC scores
+pca$li
+
+# extract PC scores for plotting
+pca_data <- as.data.frame(pca$li[,1:3])
+pca_data$Individual <- row.names(pca_data)
+colnames(pca_data) <- c("PC1", "PC2", "PC3","Individual")
+
+# plot the PCA
+library(ggplot2)
+
+ggplot(pca_data, aes(PC1, PC2)) +
+  geom_point()
+
+# read in information about the individuals
+sample_info <- read.csv("phenotype_covariates.csv")
+
+# merge the data frames together
+pca_sample_data <- merge(pca_data, sample_info, by = "Individual")
+
+pca_plot <-
+ggplot(pca_sample_data, aes(PC1, PC2, colour = Location)) +
+  geom_point()
+
+# save the plot
+jpeg("pca_plot.jpg", width = 13, height = 10, units = "cm", res = 600)
+pca_plot
+dev.off()
+
+
+
 
